b9f2a6247b022598546f9914e4c4c8764bc6c809
Mizakson <maxwellizakson@gmail.com>
2024-05-11 13:33:13 -0400

    Finish program -- number-swap.cpp

---
+++ number-swap.cpp
@@ -0,0 +1,17 @@
+// swap two numbers
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int x = 450, y = 875;
+
+    cout << "Before swapping... num1 = " << x << ", num2 = " << y << endl;
+
+    swap(x, y);
+
+    cout << "After swapping... num1 = " << x << ", num2 = " << y << endl;
+
+    return 0;
+
+}
